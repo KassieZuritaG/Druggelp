5cd2d8580cb4e97be33d8788334f3f04b2d6dfb0
CindyProyectos <cportiz3@espe.edu.ec>
2020-06-07 18:39:15 -0500

    Actualizacion entidades
    Entidades:
    Persona
    Administrador
    Cliente
    Farmaceutico

--- src/main/java/com/ortizzurita/druggelp/models/entities/Administrador.java
+++ src/main/java/com/ortizzurita/druggelp/models/entities/Administrador.java
@@ -1,5 +1,36 @@
 package com.ortizzurita.druggelp.models.entities;
 
-public class Administrador {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "administradores")
+public class Administrador extends Persona implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name="identificador")
+	private String identificador;
+
+	public Administrador() {
+		super();
+	}
+	
+	public Administrador(long id) {
+		super();
+		this.setPk_persona(id);
+	}
+
+	public String getIdentificador() {
+		return identificador;
+	}
+
+	public void setIdentificador(String identificador) {
+		this.identificador = identificador;
+	}
+	
+	
 }
--- src/main/java/com/ortizzurita/druggelp/models/entities/Cliente.java
+++ src/main/java/com/ortizzurita/druggelp/models/entities/Cliente.java
@@ -1,5 +1,34 @@
 package com.ortizzurita.druggelp.models.entities;
 
-public class Cliente {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente extends Persona implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	@Column(name="identificador")
+	private String identificador;
+
+	public Cliente() {
+		super();
+	}
+	
+	public Cliente(long id) {
+		super();
+		this.setPk_persona(id);
+	}
+
+	public String getIdentificador() {
+		return identificador;
+	}
+
+	public void setIdentificador(String identificador) {
+		this.identificador = identificador;
+	}
 }
